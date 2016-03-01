cfa29fd7e2b90b04f6c05c27298eedc4c93758eb
Sean Sellek <me@seansellek.com>
2016-02-29 19:56:09 -0500

    Supports headers and url for get requests

---
+++ JavaScriptCodeGenerator.js
@@ -0,0 +1,33 @@
+var JavaScriptCodeGenerator = function() {
+    var Mustache = require('./mustache')
+    
+    this.generate = function(context) {
+         
+        var template = readFile("request.mustache");
+        var request = context.getCurrentRequest();
+        var view = {
+            "method": request.method,
+            "url": request.url,
+            "headers": function() {
+              var headers = request.headers;
+              var array = [];
+              for (var name in headers) {
+                var header = {};
+                header.name = name;
+                header.value = headers[name].replace(/"/g, "\\\"");
+                array.push(header);
+              }
+              return array;
+            }
+        };
+
+        return Mustache.render(template, view);
+    }
+}
+
+
+JavaScriptCodeGenerator.identifier = "com.seansellek.PawExtensions.JavaScriptCodeGenerator";
+JavaScriptCodeGenerator.title = "JavaScript";
+JavaScriptCodeGenerator.fileExtension = "js";
+JavaScriptCodeGenerator.languageHighlighter = "javascript";
+registerCodeGenerator(JavaScriptCodeGenerator);
